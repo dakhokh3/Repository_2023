1eeae0d2e83ed31ce13f0dbb1e87398d6a2385d6
Дмитрий <d.xoxrin2010@yandex.ru>
2023-02-12 12:59:53 +0300

    git config --global user.name "Дмитрий Хохрин"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Ivan", "Morozov", (byte) 26);
